038f2fccd6ae43aa7e424f115b07013dc0eaa107
thidkyar <thidkyar@gmail.com>
2022-05-18 19:56:51 -0400

    implement login functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,16 +12,19 @@ import {
   PlusIcon,
   MenuIcon,
   UserIcon,
+  LoginIcon,
 } from '@heroicons/react/outline'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Header() {
+  const { data: session } = useSession()
   return (
-    <div className="sticky top-0 z-50 flex py-2 px-4 shadow-sm">
+    <div className="sticky top-0 z-50 flex items-center py-2 px-4 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0">
-        <Image src="/images/logo.png" layout="fill" objectFit="contain" />
+        <Image src="/images/logo.png" layout="fill" objectFit="contain" priority/>
       </div>
 
-      <div className="mx-5 flex items-center cursor-pointer xl:min-w-[300px]">
+      <div className="mx-5 flex cursor-pointer items-center xl:min-w-[300px]">
         <HomeIcon className="h-5 w-5" />
         <p className="ml-2 hidden flex-1 lg:inline">Home</p>
         <ChevronDownIcon className="h-5 w-5" />
@@ -48,12 +51,32 @@ function Header() {
         <PlusIcon className="icon" />
         <SpeakerphoneIcon className="icon" />
       </div>
-      <div className="flex- ml-5 items-center lg:hidden">
+
+      <div className="ml-5 flex items-center lg:hidden">
         <MenuIcon className="icon" />
       </div>
-      <div className="hidden items-center text-gray-500 lg:flex">
-        <UserIcon className="icon" />
-      </div>
+
+      {session ? (
+        <div
+          className="flex cursor-pointer items-center space-x-2 p-2 text-gray-500"
+          onClick={() => signOut()}
+        >
+          <UserIcon className="icon" />
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session?.user?.name}</p>
+            <p className="text-gray-400">1.2k Karma</p>
+          </div>
+          <ChevronDownIcon className="h-5 w-5 flex-shrink-0 text-gray-400" />
+        </div>
+      ) : (
+        <div
+          className="flex cursor-pointer items-center space-x-2 p-2 text-gray-500"
+          onClick={() => signIn()}
+        >
+          <LoginIcon className="h-5 w-5" />
+          <p>Login</p>
+        </div>
+      )}
     </div>
   )
 }
