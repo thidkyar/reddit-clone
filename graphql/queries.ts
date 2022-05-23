761399e40f9127e6dd1b3e12c8feecd65c500fbd
Taranvir Hidkyar <thidkyar@gmail.com>
2022-05-23 01:08:54 -0400

    create post and feed components / setup db queries for respestive components

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,5 +1,38 @@
 import { gql } from '@apollo/client'
 
+export const GET_ALL_POSTS = gql`
+  query MyQuery {
+    getPostList {
+      id
+      body
+      created_at
+      image
+      subreddit_id
+      title
+      username
+      subreddits {
+        created_at
+        id
+        topic
+      }
+      comments {
+        id
+        comment
+        username
+        post_id
+        created_at
+      }
+      votes {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
+    }
+  }
+`
+
 export const GET_SUBREDDIT_BY_TOPIC = gql`
   query MyQuery($topic: String!) {
     getSubredditListByTopic(topic: $topic) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import PostBox from '../components/Postbox'
 
 const Home: NextPage = () => {
@@ -12,8 +13,9 @@ const Home: NextPage = () => {
 
       {/* POSTBOX */}
       <PostBox />
-      <div>
+      <div className="">
         {/* feed */}
+        <Feed />
       </div>
     </div>
   )
